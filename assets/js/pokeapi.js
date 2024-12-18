6a445167b40288f8c3ec93b7fec1d08c7a23cb48
Cybervtx <149208626+Cybervtx@users.noreply.github.com>
2024-12-18 12:14:40 +0000

    code

--- assets/js/pokeapi.js
+++ assets/js/pokeapi.js
@@ -1,10 +1,16 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonUrl = (pokemon) => {
+    return fetch(pokemon.url).then((res) => res.json())
+}
+
 pokeApi.getPokemons = (offset=0,limit=12) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
     .then((res) => res.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error())
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonUrl))
+    .then((detailsReq) => Promise.all(detailsReq))
+    .then((pokemonDetails) => pokemonDetails)
 }
